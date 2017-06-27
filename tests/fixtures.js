25a951fdc79b74fe78d752e16c9e134d7cd40007
Will Engler <Engler.Will@gmail.com>
2017-06-27 13:00:40 -0500

    Make harness to run locally

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -1,95 +1,111 @@
-exports.smallTree = {
-    network1: {
-        node1: {
-            sensor1: {
-                nickname1: 'temperature.temperature',
-                nickname2: 'relative_humidity.humidity'
-            },
-            sensor2: {
-                nickname1: 'magnetic_field.y',
-                nickname2: 'magnetic_field.x',
-                nickname3: 'magnetic_field.z'
-            }
-        },
-        node2: {
-            sensor2: {
-                nickname1: 'magnetic_field.y',
-                nickname2: 'magnetic_field.x',
-                nickname3: 'magnetic_field.z'
-            },
-            sensor3: {
-                nickname1: "atmospheric_pressure.pressure",
-                nickname2: "temperature.temperature"
-            }
-        }
+exports.localTestTree = {
+  array_of_things_chicago: {
+    "0000001e0610b9e7": {
+      bmi160: {
+        accel_z: "acceleration.z",
+        accel_x: "acceleration.x",
+        accel_y: "acceleration.y",
+        orient_z: "orientation.z",
+        orient_x: "orientation.x",
+        orient_y: "orientation.y"
+      },
+      tmp112: { temperature: "temperature.temperature" }
     }
+  }
+};
+
+exports.smallTree = {
+  network1: {
+    node1: {
+      sensor1: {
+        nickname1: "temperature.temperature",
+        nickname2: "relative_humidity.humidity"
+      },
+      sensor2: {
+        nickname1: "magnetic_field.y",
+        nickname2: "magnetic_field.x",
+        nickname3: "magnetic_field.z"
+      }
+    },
+    node2: {
+      sensor2: {
+        nickname1: "magnetic_field.y",
+        nickname2: "magnetic_field.x",
+        nickname3: "magnetic_field.z"
+      },
+      sensor3: {
+        nickname1: "atmospheric_pressure.pressure",
+        nickname2: "temperature.temperature"
+      }
+    }
+  }
 };
 
 exports.formattedTree = {
-    network1: {
-        node1: {
-            sensor1: {
-                temperature: null,
-                relative_humidity: null
-            },
-            sensor2: {
-                magnetic_field: null
-            }
-        },
-        node2: {
-            sensor2: {
-                magnetic_field: null
-            },
-            sensor3: {
-                temperature: null,
-                atmospheric_pressure: null
-            }
-        }
+  network1: {
+    node1: {
+      sensor1: {
+        temperature: null,
+        relative_humidity: null
+      },
+      sensor2: {
+        magnetic_field: null
+      }
+    },
+    node2: {
+      sensor2: {
+        magnetic_field: null
+      },
+      sensor3: {
+        temperature: null,
+        atmospheric_pressure: null
+      }
     }
+  }
 };
 
 exports.smallTree2 = {
-    network1: {
-        node1: {
-            sensor1: {
-                nickname1: 'temperature.temperature',
-                nickname2: 'relative_humidity.humidity'
-            },
-            sensor2: {
-                nickname1: 'foo.bar'
-            }
-        },
-        node2: {
-            sensor2: {
-                nickname1: 'foo.bar'
-            },
-            sensor3: {
-                nickname1: "atmospheric_pressure.pressure",
-                nickname2: "temperature.temperature"
-            }
-        }
+  network1: {
+    node1: {
+      sensor1: {
+        nickname1: "temperature.temperature",
+        nickname2: "relative_humidity.humidity"
+      },
+      sensor2: {
+        nickname1: "foo.bar"
+      }
+    },
+    node2: {
+      sensor2: {
+        nickname1: "foo.bar"
+      },
+      sensor3: {
+        nickname1: "atmospheric_pressure.pressure",
+        nickname2: "temperature.temperature"
+      }
     }
+  }
 };
 
 exports.formattedTree2 = {
-    network1: {
-        node1: {
-            sensor1: {
-                temperature: null,
-                relative_humidity: null
-            },
-            sensor2: {
-                foo: null
-            }
-        },
-        node2: {
-            sensor2: {
-                foo: null
-            },
-            sensor3: {
-                temperature: null,
-                atmospheric_pressure: null
-            }
-        }
+  network1: {
+    node1: {
+      sensor1: {
+        temperature: null,
+        relative_humidity: null
+      },
+      sensor2: {
+        foo: null
+      }
+    },
+    node2: {
+      sensor2: {
+        foo: null
+      },
+      sensor3: {
+        temperature: null,
+        atmospheric_pressure: null
+      }
     }
+  }
 };
--- tests/testServer.js
+++ tests/testServer.js
@@ -1,22 +1,45 @@
-const { setUpRedis, setUpSocketIo } = require("../app/pubsub.js");
-const {sensorTree} = require('./fixtures');
+const {SensorTreeCache} = require('../app/sensorTreeCache.js');
+const {listenForRecords, listenForSubscribers} = require('../app/pubsub.js');
+const {localTestTree} = require('./fixtures.js');
 
-const redis = require("redis").createClient({
-  host: process.env.REDIS_HOST || "localhost",
-  port: 6379
+const redis = require('redis').createClient({
+    host: process.env.REDIS_HOST || 'localhost', 
+    port: 6379,
+    retry_strategy
 });
+
+function retry_strategy(retryInfo) {
+    // If we've been at this for more than 10 seconds
+    // kill this process to give Elastic Beanstalk a chance at rebooting us.
+    if (retryInfo.total_retry_time > (1000 * 10)) {
+        console.log('Fatal error: Lost connection to Redis.');
+        process.exit(1);
+    }
+    // Wait one second between attempts.
+    return 1000;
+}
 
 const app = require("http").createServer();
 const io = require("socket.io")(app, {
-  transports: ["websocket"]
+    transports: ['websocket']
 });
 
+const packageResult = sensor_tree => Promise.resolve({rows: [{sensor_tree}]});
 const pgClient = {
   query(statement) {
-    return  Promise.resolve(sensorTree);
+    return packageResult(localTestTree);
   }
 };
 
-setUpSocketIo(pgClient, io)
-  .then(() => setUpRedis(redis, io))
-  .then(() => app.listen(8081));
+const sensorTreeCache = new SensorTreeCache(pgClient);
+sensorTreeCache.seed()
+.catch(err => {
+    // Abort on error if we fail to initiate the sensor tree cache
+    console.log(`Fatal error: could not initialize sensor metadata: ${err}`, err.stack);
+    process.exit(1);
+})
+.then(cache => {
+    listenForSubscribers(cache, io);
+    listenForRecords(cache, io, redis);
+    app.listen(8081);
+});
